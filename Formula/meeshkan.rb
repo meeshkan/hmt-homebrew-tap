15618d723309e753634cd440b26d450d6858f3ec
Fredrik Fornwall <fredrik@fornwall.net>
2020-03-10 12:10:47 +0100

    Bump revision

--- Formula/meeshkan.rb
+++ Formula/meeshkan.rb
@@ -5,7 +5,7 @@ class Meeshkan < Formula
   homepage "https://github.com/meeshkan/meeshkan"
   url "https://github.com/meeshkan/meeshkan/archive/v0.2.16.tar.gz"
   sha256 "48db04a893ebb9df8f586921cd8e24f633a7a5a5c630d244ea74dd23511e59b3"
-  revision 1
+  revision 2
   head "https://github.com/meeshkan/meeshkan.git"
 
   depends_on "python@3.8"
