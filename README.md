e58c664321a65ec52b42852ed64da0ced0ca4ab6
Mike Solomon <mike@meeshkan.com>
2020-05-05 15:11:25 +0300

    Modifies package to have HMT

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Homebrew tap for meeshkan
-A homebrew tap to install the [meeshkan](https://github.com/meeshkan/meeshkan) schema builder and mocking tool using [Homebrew](https://brew.sh/):
+# Homebrew tap for hmt
+A homebrew tap to install the [hmt](https://github.com/meeshkan/hmt) schema builder and mocking tool using [Homebrew](https://brew.sh/):
 
 ```sh
-brew tap meeshkan/tap
-brew install meeshkan
+brew tap hmt/tap
+brew install hmt
 ```
 
 # For developers
 
-Run `./generate-formula.sh` to update [Formula/meeshkan.rb](Formula/meeshkan.rb) with the latest version published to PyPi.
+Run `./generate-formula.sh` to update [Formula/hmt.rb](Formula/hmt.rb) with the latest version published to PyPi.
 
-Run `brew reinstall --build-from-source Formula/meeshkan.rb` to test installing the package locally before pushing.
+Run `brew reinstall --build-from-source Formula/hmt.rb` to test installing the package locally before pushing.
--- generate-formula.sh
+++ generate-formula.sh
@@ -3,32 +3,32 @@
 set -e -u
 
 echo "Installing using pip..."
-pip install --upgrade homebrew-pypi-poet meeshkan
+pip install --upgrade homebrew-pypi-poet hmt
 
-MEESHKAN_VERSION=`pip show meeshkan | grep Version: | cut -f 2 -d ' '`
-echo "meeshkan version: $MEESHKAN_VERSION"
-MEESHKAN_TARFILE=v$MEESHKAN_VERSION.tar.gz
-MEESHKAN_URL=https://github.com/meeshkan/meeshkan/archive/$MEESHKAN_TARFILE
-echo "Downloading $MEESHKAN_URL ..."
-curl -L -O $MEESHKAN_URL
-MEESHKAN_CHECKSUM=`sha256sum $MEESHKAN_TARFILE | cut -f 1 -d ' '`
+hmt_VERSION=`pip show hmt | grep Version: | cut -f 2 -d ' '`
+echo "hmt version: $hmt_VERSION"
+hmt_TARFILE=v$hmt_VERSION.tar.gz
+hmt_URL=https://github.com/meeshkan/hmt/archive/$hmt_TARFILE
+echo "Downloading $hmt_URL ..."
+curl -L -O $hmt_URL
+hmt_CHECKSUM=`sha256sum $hmt_TARFILE | cut -f 1 -d ' '`
 
-OUTPUT=Formula/meeshkan.rb
+OUTPUT=Formula/hmt.rb
 
 cat > $OUTPUT <<EOF
-class Meeshkan < Formula
+class hmt < Formula
   include Language::Python::Virtualenv
 
   desc "Mock HTTP APIs"
-  homepage "https://github.com/meeshkan/meeshkan"
-  url "$MEESHKAN_URL"
-  sha256 "$MEESHKAN_CHECKSUM"
-  head "https://github.com/meeshkan/meeshkan.git"
+  homepage "https://github.com/meeshkan/hmt"
+  url "$hmt_URL"
+  sha256 "$hmt_CHECKSUM"
+  head "https://github.com/meeshkan/hmt.git"
 
   depends_on "python@3.8"
 EOF
 
-poet meeshkan >> $OUTPUT
+poet hmt >> $OUTPUT
 
 cat >> $OUTPUT <<EOF
   def install
